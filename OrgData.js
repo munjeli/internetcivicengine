8ee4bd644e015462a984f80424e3f11b98f8bb88
Saar Picker <saarp@saarp-laptop.(none)>
2013-08-10 16:46:04 -0700

    Org data.

---
+++ OrgData.js
@@ -0,0 +1,113 @@
+var orgs = [
+{ 
+name: "Seattle City Club", 
+address: "1333 5th Ave #24 Seattle, WA 98101", 
+url: "http://www.seattlecityclub.org/", 
+categories: ["individual impact", "education"], 
+}, { 
+name: "WA Bus", 
+address: "1100 E. Union Unit 1E, Seattle, WA 98112", 
+url: "http://washingtonbus.org/", 
+categories: ["political", "Voter Education"], 
+}, { 
+name: "The Vera Project", 
+address: "Warren Avenue North Seattle, WA 98109", 
+url: "http://theveraproject.org/", 
+categories: ["group impact", "Youth"], 
+}, { 
+name: "Teachers United", 
+address: "210 South Hudson St. #326 Seattle, Washington, 98134", 
+url: "http://www.teachersunitedwa.org/", 
+categories: ["political issues", "education"], 
+}, { 
+name: "Road Map Project", 
+address: "2100 24th Avenue South Suite 340 Seattle, WA 98144", 
+url: "http://www.roadmapproject.org/", 
+categories: ["political issues", "education"], 
+}, { 
+name: "Ashoka Seattle", 
+address: "220 2nd Ave South Suite 102", 
+url: "http://www.ashokaseattle.org/", 
+categories: ["group impact", "youth"], 
+}, { 
+name: "Seattle Foundation", 
+address: "1200 Fifth Avenue, Suite 1300?Seattle, WA 98101-3151 ", 
+url: "http://www.seattlefoundation.org/Pages/Default.aspx", 
+categories: ["everything"], 
+}, { 
+name: "Bread of Life Mission", 
+address: "97 S Main St. Seattle, WA 98104", 
+url: "http://www.breadoflifemission.org/", 
+categories: ["poverty", "hunger"], 
+}, { 
+name: "Puget Soundoff", 
+address: "909 Fourth Avem Seattle, WA 98104", 
+url: "http://pugetsoundoff.org/", 
+categories: ["individual impact", "youth"], 
+}, { 
+name: "Treehouse", 
+address: "2100 24th Avenue South Suite 200 Seattle, WA 98144", 
+url: "http://www.treehouseforkids.org/", 
+categories: ["individual impact", "youth", "homelessness"], 
+}, { 
+name: "Millionair Club Charity", 
+address: "2515 Western Avenue Seattle, WA 98121", 
+url: "http://millionairclub.org/", 
+categories: ["individual impact", "poverty"], 
+}, { 
+name: "Seattle Works", 
+address: "1625 19th Ave Seattle, WA 98122", 
+url: "http://www.seattleworks.org/", 
+categories: ["group impact", "everything"], 
+}, { 
+name: "Mona Foundation", 
+address: "218 Main St. Suite 404 Kirkland, WA 98033", 
+url: "https://www.monafoundation.org/", 
+categories: ["group impact", "education"], 
+}, { 
+name: "America Scores Seattle", 
+address: "2450 6th Ave S, Suite 203 Seattle, WA 98134", 
+url: "http://www.americascores.org/affiliates/seattle", 
+categories: ["individual impact", "education"], 
+}, { 
+name: "Alliance of People with Disabilities", 
+address: "1120 E Terrace St., Suite 100 Seattle, WA 98122", 
+url: "http://disabilitypride.org/wordpress/", 
+categories: ["Mental Illness / disabilities"], 
+}, { 
+name: "Alliance for Education", 
+address: "509 Olive Way, Suite 500 Seattle, WA 98101-2556", 
+url: "http://alliancefored.org/", 
+categories: ["political issues", "education"], 
+}, { 
+name: "YouthCare ", 
+address: "1828 Yale Ave. Seattle, WA 98101", 
+url: "http://www.youthcare.org/", 
+categories: ["individual impact", "youth", "homelessness", "poverty"], 
+}, { 
+name: "King County Sexual Assault Resource Center", 
+address: "200 Mill Ave. South Suite 10 Renton, WA 98057", 
+url: "http://www.kcsarc.org/", 
+categories: ["individual impact", "domestic violence"], 
+}, { 
+name: "Town Hall Seattle", 
+address: "1119 8th Ave. Seattle, WA 98101", 
+url: "http://townhallseattle.org/", 
+categories: ["political", "local politics"], 
+}, { 
+name: "Transportation Choices Coalition", 
+address: "219 1st Ave S Suite 420 Seattle, WA 98104", 
+url: "http://transportationchoices.org/", 
+categories: ["group impact", "transportation"], 
+}, { 
+name: "Washington Allliance for Gun Responsibility ", 
+address: "", 
+url: "http://wagunresponsibility.org", 
+categories: ["political", "state politics"], 
+}, { 
+name: "Seattle Young People's Project", 
+address: "510B Maynard Ave S, Seattle WA 98104", 
+url: "http://sypp.org/", 
+categories: ["group impact", "racial issues"], 
+} 
+];
